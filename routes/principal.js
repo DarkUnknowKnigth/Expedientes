45a13e120802631cebafc839f462b439035efd5a
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-05-19 01:26:07 -0500

    actualizando direcciones

--- routes/principal.js
+++ routes/principal.js
@@ -5,9 +5,9 @@ var address='https://stark-sea-10471.herokuapp.com';
 principal.get("/:id&:user&:pass",(req,res)=>{
     res.send({message:
         '<center>'+
-        '<span class="badge badge-success">'+
-            '<h1>Bienvenido! al Sistema: <br> '+req.params.user+
-       '</h1></span>'+
+        '<span class="badge badge-success border-dark">'+
+            '<h1>Bienvenido al Sistema: <br> '+req.params.user+
+       '!</h1></span>'+
     '</center>',url:address+"/modulo/"+req.params.id+"&"+req.params.user+"&"+req.params.pass});
 }); 
 module.exports=principal;

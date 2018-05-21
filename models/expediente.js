b3fc27c9512c0299d78aa7a3c6c17edf4ab5f4d1
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-05-21 07:12:42 -0500

    actualizado

--- models/expediente.js
+++ models/expediente.js
@@ -5,39 +5,39 @@ var Schema=mongoose.Schema;
 var ExpedienteSchema= Schema({
     AntecedentesHF:[
             {
-                parentesco:{type:String,required: true},
-                HA:{type:String,required: true},
-                cancer:{type:String,required: true},
-                sida:{type:String,required: true},
-                diabetes:{type:String,required: true},
-                TB:{type:String,required: true},
-                otro:{type:String,required: true}
+                parentesco:{type:String,required: false},
+                HA:{type:String,required: false},
+                cancer:{type:String,required: false},
+                sida:{type:String,required: false},
+                diabetes:{type:String,required: false},
+                TB:{type:String,required: false},
+                otro:{type:String,required: false}
             }
     ]
     ,
     FichaId:{
-        nombre:{type:String,required: true},
-        apPaterno:{type:String,required: true},
-        apMaterno:{type:String,required: true},
-        direccion:{type:String,required: true},
-        curp:{type:String,required: true},
-        estadoCivil:{type:String,required: true},
-        ocupacion:{type:String,required: true}
+        nombre:{type:String,required: false},
+        apPaterno:{type:String,required: false},
+        apMaterno:{type:String,required: false},
+        direccion:{type:String,required: false},
+        curp:{type:String,required: false},
+        estadoCivil:{type:String,required: false},
+        ocupacion:{type:String,required: false}
     },
     APNP:{
-        promiscuidad:{type:String,required: true},
-        tabaco:{type:String,required: true},
-        alcohol:{type:String,required: true},
-        fauna:{type:String,required: true},
-        hacinamiento:{type:String,required: true},
-        dieta:{type:String,required: true},
-        vivienda:{type:String,required: true}
+        promiscuidad:{type:String,required: false},
+        tabaco:{type:String,required: false},
+        alcohol:{type:String,required: false},
+        fauna:{type:String,required: false},
+        hacinamiento:{type:String,required: false},
+        dieta:{type:String,required: false},
+        vivienda:{type:String,required: false}
     },
     Inmunizaciones:{
-        sabin:{type:String,required: true},
-        bcg:{type:String,required: true},
-        dpt:{type:String,required: true},
-        antisarampion:{type:String,required: true}
+        sabin:{type:String,required: false},
+        bcg:{type:String,required: false},
+        dpt:{type:String,required: false},
+        antisarampion:{type:String,required: false}
     }
     ,
     APP:{

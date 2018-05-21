4bba3846bc08fbda2f6475984e7fdd6a31dd94e9
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-05-21 07:01:14 -0500

    actualizado

--- controllers/expediente.js
+++ controllers/expediente.js
@@ -11,33 +11,34 @@ function eliminar(req,res)
 function guardar(req,res){
     var newExp=req.body;
     console.log(newExp);
-    // {
-    //     FichaId:newExp.FichaId,
-    //     APNP:newExp.APNP,
-    //     Inmunizaciones:newExp.Inmunizaciones,
-    //     APP:newExp.APP,
-    //     AntecedentesG:newExp.AntecedentesG
-    // }
-    var expediente= new Expediente();
+    
+    var expediente= new Expediente({
+        'AntecedentesHF':newExp.AntecedentesHF,
+        'FichaId':newExp.FichaId,
+        'APNP':newExp.APNP,
+        'Inmunizaciones':newExp.Inmunizaciones,
+        'APP':newExp.APP,
+        'AntecedentesG':newExp.AntecedentesG
+    });
     console.log("recibi");
     console.log(expediente);
-    for (let i = 0; i < expediente.AntecedentesHF.length; i++) {
+    // for (let i = 0; i < expediente.AntecedentesHF.length; i++) {
         
-        expediente.AntecedentesHF[i].parentesco=newExp.AntecedentesHF[i].parentesco;
-        expediente.AntecedentesHF[i].HA=newExp.AntecedentesHF[i].HA;
-        expediente.AntecedentesHF[i].cancer=newExp.AntecedentesHF[i].cancer;
-        expediente.AntecedentesHF[i].sida=newExp.AntecedentesHF[i].sida;
-        expediente.AntecedentesHF[i].diabetes=newExp.AntecedentesHF[i].diabetes;
-        expediente.AntecedentesHF[i].TB=newExp.AntecedentesHF[i].TB;
-        expediente.AntecedentesHF[i].otro=newExp.AntecedentesHF[i].otro;
-    }
-    expediente.FichaId.nombre=newExp.FichaId.nombre;
-    expediente.FichaId.apPaterno=newExp.FichaId.apPaterno;
-    expediente.FichaId.apMaterno=newExp.FichaId.apMaterno;
-    expediente.FichaId.direccion=newExp.FichaId.direccion;
-    expediente.FichaId.curp=newExp.FichaId.curp;
-    expediente.FichaId.estadoCivil=newExp.FichaId.estadoCivil;
-    expediente.FichaId.ocupacion=newExp.FichaId.ocupacion;
+    //     expediente.AntecedentesHF[i].parentesco=newExp.AntecedentesHF[i].parentesco;
+    //     expediente.AntecedentesHF[i].HA=newExp.AntecedentesHF[i].HA;
+    //     expediente.AntecedentesHF[i].cancer=newExp.AntecedentesHF[i].cancer;
+    //     expediente.AntecedentesHF[i].sida=newExp.AntecedentesHF[i].sida;
+    //     expediente.AntecedentesHF[i].diabetes=newExp.AntecedentesHF[i].diabetes;
+    //     expediente.AntecedentesHF[i].TB=newExp.AntecedentesHF[i].TB;
+    //     expediente.AntecedentesHF[i].otro=newExp.AntecedentesHF[i].otro;
+    // }
+    // expediente.FichaId.nombre=newExp.FichaId.nombre;
+    // expediente.FichaId.apPaterno=newExp.FichaId.apPaterno;
+    // expediente.FichaId.apMaterno=newExp.FichaId.apMaterno;
+    // expediente.FichaId.direccion=newExp.FichaId.direccion;
+    // expediente.FichaId.curp=newExp.FichaId.curp;
+    // expediente.FichaId.estadoCivil=newExp.FichaId.estadoCivil;
+    // expediente.FichaId.ocupacion=newExp.FichaId.ocupacion;
     //expediente.AntecedentesHF=newExp.AntecedentesHF;
     // expediente.FichaId=newExp.FichaId;
     // expediente.APNP=newExp.APNP;

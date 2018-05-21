80388ea98e600af90ce59c74034695f9ff9b8653
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-05-21 04:19:56 -0500

    actualizado

--- models/cobertura.js
+++
@@ -1,9 +0,0 @@
-var mongoose=require("mongoose");
-var Schema=mongoose.Schema;
-var CoberturaSchema= Schema({
-    PrimeraVezAnio:String,
-    IMC_10_19a:String,
-    SintRespTB:Boolean
-    });
-
-    module.exports=mongoose.model('Cobertura',CoberturaSchema);

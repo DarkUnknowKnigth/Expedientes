edb8b15695ade57a44b1d013566316132cd52b8b
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-05-22 04:15:55 -0500

    actualizado

--- controllers/expediente.js
+++ controllers/expediente.js
@@ -14,22 +14,22 @@ function guardar(req, res) {
     var expediente = new Expediente({
         AntecedentesHF: [
             {
-                parentesco: nuevo.AntecedentesHF[0][parentesco],
-                HA: nuevo.AntecedentesHF[0][HA],
-                cancer: nuevo.AntecedentesHF[0][cancer],
-                sida: nuevo.AntecedentesHF[0][sida],
-                diabetes: nuevo.AntecedentesHF[0][diabetes],
-                TB: nuevo.AntecedentesHF[0][TB],
-                otro: nuevo.AntecedentesHF[0][otro]
+                parentesco: nuevo.AntecedentesHF[0]['parentesco'],
+                HA: nuevo.AntecedentesHF[0]['HA'],
+                cancer: nuevo.AntecedentesHF[0]['cancer'],
+                sida: nuevo.AntecedentesHF[0]['sida'],
+                diabetes: nuevo.AntecedentesHF[0]['diabetes'],
+                TB: nuevo.AntecedentesHF[0]['TB'],
+                otro: nuevo.AntecedentesHF[0]['otro']
             },
             {
-                parentesco: nuevo.AntecedentesHF[1][parentesco],
-                HA: nuevo.AntecedentesHF[1][HA],
-                cancer: nuevo.AntecedentesHF[1][cancer],
-                sida: nuevo.AntecedentesHF[1][sida],
-                diabetes: nuevo.AntecedentesHF[1][diabetes],
-                TB: nuevo.AntecedentesHF[1][TB],
-                otro: nuevo.AntecedentesHF[1][otro]
+                parentesco: nuevo.AntecedentesHF[1]['parentesco'],
+                HA: nuevo.AntecedentesHF[1]['HA'],
+                cancer: nuevo.AntecedentesHF[1]['cancer'],
+                sida: nuevo.AntecedentesHF[1]['sida'],
+                diabetes: nuevo.AntecedentesHF[1]['diabetes'],
+                TB: nuevo.AntecedentesHF[1]['TB'],
+                otro: nuevo.AntecedentesHF[1]['otro']
             }
         ],
         nombre: nuevo.nombre,

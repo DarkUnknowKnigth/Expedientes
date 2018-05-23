db234ec5ff18dad7faa6d002df86fbd1624a4b0e
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-05-23 00:04:55 -0500

    actualizado

--- controllers/expediente.js
+++ controllers/expediente.js
@@ -86,11 +86,11 @@ function guardar(req, res) {
         if(err)
         {
             console.log("ERROR:"+err);
-            res.send({msg:"Ha ocurrido un erro en el guardado, porfavor intente de nuevo",url:req.baseUrl.replace("/expedientes/nuevoExpediente","")});
+            res.send({msg:"Ha ocurrido un erro en el guardado, porfavor intente de nuevo",url:req.baseUrl.replace("/expedientes","")});
         }
         else{
 
-            res.send({msg:"Expedeinte creado Exitosamente1 !",url:req.baseUrl.replace("/expedientes/nuevoExpediente","")});
+            res.send({msg:"Expedeinte creado Exitosamente1 !",url:req.baseUrl.replace("/expedientes","")});
         }
     });
     //console.log(expediente);

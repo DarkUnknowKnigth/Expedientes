6a769595f514601fb7e532629f3d2d89c3222616
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-05-26 22:22:39 -0500

    actualizado

--- public/js/GeClinic.js
+++ public/js/GeClinic.js
@@ -409,7 +409,7 @@ $("#guardarUsuario").click((e)=>{
 	var tu=$("#tipoUser");
 	var cp=$("#cedulaProfesional");
 	var a=$("#activeUser");
-	if(!/\s/.test(u))
+	if(!/\s/.test(u.val()))
 	{
 		$.ajax({
 			method:"POST",

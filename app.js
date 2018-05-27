8117b29cafba850d09c7f726818c5ec81309f9dd
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-05-26 22:07:01 -0500

    actualizado

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use(session({
 ============================================================*/
 //ruta base carga vista del login es ala que se accede al cargar la pagina por primera vez
 app.get("/",(req,res)=>{
+    req.session.user_id=null;
     res.render("pages/login");
 });
 app.use((req,res,next)=>{ //middleware de session

0d7ba8c653b4adf9dfbfdaecb519c4650c4cca69
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-06-04 17:13:19 -0500

    actualizado

--- controllers/coordinador.js
+++ controllers/coordinador.js
@@ -10,7 +10,7 @@ function generarHojaDiaria(req,res)
     Consulta.find({"diagnostico":{"fecha":{$date:new Date(fecha)}}}).exec((err,consultas)=>{
         if(!err)
         {
-            if(consultas)
+            if(consultas.length>0)
             {
                 tb='<table class="table table-striped"><tr><td colspan="6">Signos vitales</td><td colspan="5">Informacion de consulta</td><td colspan="3">Diagnostico</td></tr>'+
                     '<tr><td>Talla</td><td>Peso</td><td>IMC</td><td>Pulso</td><td>TA</td><td>Temperatura</td>'+

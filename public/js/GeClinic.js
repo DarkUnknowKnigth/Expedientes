11f775d38af4964509cd7ab39541735396ccd9e3
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-06-07 09:21:16 -0500

    actualizado

--- public/js/GeClinic.js
+++ public/js/GeClinic.js
@@ -501,28 +501,14 @@ $("#generarHojaDiaria").click((e)=>{
 						$(this).dialog("close");
 					},
 					Guardar:()=>{
-					
-						//getting values of current time for generating the file name
-						var dt = new Date();
-						var day = dt.getDate();
-						var month = dt.getMonth() + 1;
-						var year = dt.getFullYear();
-						var hour = dt.getHours();
-						var mins = dt.getMinutes();
-						var postfix = day + "." + month + "." + year + "_" + hour + "." + mins;
-						//creating a temporary HTML link element (they support setting file names)
-						var a = document.createElement('a');
-						//getting data from our div that contains the HTML table
-						var data_type = 'data:application/vnd.ms-excel';
-						var table_div = $("#HojaDiaria_clinica");
-						var table_html = table_div.outerHTML.replace(/ /g, '%20');
-						a.href = data_type + ', ' + table_html;
-						//setting the file name
-						a.download = 'exported_table_' + postfix + '.xls';
-						//triggering the function
-						a.click();
-						//just in case, prevent default behaviour
-						e.preventDefault();
+						var table= $("#HojaDiaria_clinica");
+  						var html = table.html();
+						//window.open('data:application/vnd.ms-excel,' + encodeURIComponent(html));
+						var result = "data:application/vnd.ms-excel,"+encodeURIComponent(html);
+						this.href = result;
+						var now=new Date();
+						this.download = "HojaDiaria_"+now.toDateString()+".xls";
+						return true;
 					}
 				}
 			});

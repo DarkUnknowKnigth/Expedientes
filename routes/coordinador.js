88ec2a18e2a567c98b6fbe39a0e75d0eabac576f
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-06-07 01:12:05 -0500

    actualizado

--- routes/coordinador.js
+++ routes/coordinador.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const controladorCoordinador=require('../controllers/coordinador');
 const coordinador=express.Router();
-dinador.post("/generaInforme",controladorCoordinador.generarInforme);
 coordinador.post("/generarHojaDiaria",controladorCoordinador.generarHojaDiaria);
 coordinador.post("/estadistica",controladorCoordinador.estadistica);
 coordinador.post("/anual",controladorCoordinador.anual);

0472d0d6a8f394efff4fe70ce64f96151319483f
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-06-07 11:20:25 -0500

    actualizado

--- public/js/GeClinic.js
+++ public/js/GeClinic.js
@@ -504,7 +504,7 @@ $("#generarHojaDiaria").click((e)=>{
 						var a = document.createElement('a');
 						var table= $("#HojaDiaria_clinica").html();
 						// window.open('data:application/vnd.ms-excel,' + encodeURIComponent(table));
-						var result = "data:application/vnd.ms-excel,"+encodeURIComponent(table.replace("ó","o").replace("á","a"));
+						var result = "data:application/vnd.ms-excel,"+encodeURIComponent(table.replace("ón","on").replace("á","a").replace('í','i'));
 						a.href = result;
 						a.download = "HojaDiaria_"+$("#fechaHoja").val()+".xls";
 						a.click();

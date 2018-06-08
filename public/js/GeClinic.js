38a528984d3c2b8da71fe5620c3e4a7aca9d53dc
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-06-07 20:12:29 -0500

    actualizado

--- public/js/GeClinic.js
+++ public/js/GeClinic.js
@@ -1207,5 +1207,5 @@ $(".modal.continuar3 .modal-footer .btn-success").click(function(e){
 $("#nextAntec").click(()=>{
 
 });
-
+$("input ").keyup((e)=>{/[a-zA-Z]/.test(e.target.value) ? true : e.preventDefault()});
 ///campos seguros

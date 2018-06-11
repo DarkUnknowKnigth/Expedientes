9d3812caabdef9ca3cc96b3096c653e7583e2719
DarkUnknowKnigth <jose-daniel-97@hotmail.com>
2018-06-11 00:05:06 -0500

    actualizado

--- controllers/session.js
+++ controllers/session.js
@@ -2,7 +2,7 @@ var Ss=require('../models/session');
 var Us=require('../models/usuario');
 function guardarSession(id)
 {
-    Us.findById({id}).exec((err,user)=>{
+    Us.findOne({id}).exec((err,user)=>{
         if(!err && !estaRegistrado(id))
         {
             var uss=new Ss();
